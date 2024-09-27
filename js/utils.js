27286b864449d3e72dd0011ecdfcde30698d8ff0
Soubhik <soubhiks@hotmail.com>
2024-09-27 22:08:34 +0100

    dynamically loading the phrases.js files from utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,17 @@
 
+// Function to load the scripts for the selected language
+const scripts = [
+    "Polish/phrases.js",
+    "Bengali/phrases.js",
+    "Tamil/phrases.js",
+    "French/phrases.js"
+];
+
+scripts.forEach(src => {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+});
 
 // Function to highlight the active button
 function highlightButton(button) {
